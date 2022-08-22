975fabc930543fc30b7e99d0ab2ee4b60b18c8d6
Frederick Clay <fredfmclay@gmail.com>
2022-08-22 12:55:53 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
+import bagEndSketch from './ImagesToUse/bagEndSketch.jpg'
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Lotr from './Lotr';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div id='middleSection'>
+        <Lotr/>
+        <img src={bagEndSketch} alt='bag end drawing'></img>
+      </div>
+      <Footer/>
     </div>
   );
+  
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
